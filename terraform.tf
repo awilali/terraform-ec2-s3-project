1e7bb5b5f9b9d44d4004d13237d189df0e1c9d7d
Awil Ali <60300580+Yeyle@users.noreply.github.com>
2024-04-15 20:44:28 +0100

    Create terraform.tf
    I'd normally have 2 separate files terraform.tf with just the terraform block and provider.tf with the provider block, but I've combined them in this project.

---
+++ terraform.tf
@@ -0,0 +1,17 @@
+#This is the terraform.tf block, which shows the terraform version and our cloud provider.
+#Further down there's the provider block, which shows our cloud provider, region and aws credentials (I have deleted the keys for security reason).
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-2"
+    access_key = "access_key"
+    secret_key = "secret_key"
+}
